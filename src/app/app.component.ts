2250fd76ed0164d471e41dce2c97f7ee2fa5212f
durlavkalita <durlavk98@gmail.com>
2024-06-22 18:26:05 +0530

    basic functionality + styling added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WeatherComponent } from './weather/weather.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [RouterOutlet, WeatherComponent],
 })
 export class AppComponent {
   title = 'weather-dashboard';
+  currentWeather: any;
+  weatherForecast: any;
 }
